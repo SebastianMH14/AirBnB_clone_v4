db53287aa5839863a871f3e06a2acfaee4d647d6
jdcera4 <jdcera4@gmail.com>
2021-09-24 14:42:43 -0400

    status api available

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,4 +13,11 @@ $(function () {
       $('.amenities h4').text(amenitiesName.join(', '));
     });
 });
-  
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  if (data.status === 'OK') {
+    $('DIV#api_status').addClass('available');
+  } else {
+    $('DIV#api_status').removeClass('avalable');
+  }
+});
